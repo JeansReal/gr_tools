ba468dd411b5c299c93e4a4c3f1684af04d2c79d
Jeans K. Real <jkrb7@hotmail.com>
2024-04-29 20:15:09 -0600

    Payment Entry Work in Progress, Only for QB?

---
+++ gr_tools/public/js/doctype/payment_entry.js
@@ -0,0 +1,29 @@
+// TODO: Work in Progress!
+frappe.ui.form.on("Payment Entry", {
+
+	refresh(frm) {
+		console.log('REFRESH!');
+
+		frappe.db.get_list('Mode of Payment').then((results) => {
+
+			let custom_control_df = Object.assign(frm.fields_dict['mode_of_payment'].df, {
+				fieldtype: 'MultiCheckSingle', columns: 2, options: results.map(c => c.name),
+				on_change: (selected) => frm.set_value('mode_of_payment', selected)
+			});
+
+			frappe.ui.form.make_control({
+				parent: frm.fields_dict.mode_of_payment.$wrapper,
+				df: custom_control_df,
+				render_input: true,
+			});
+
+		});
+	},
+
+	posting_date(frm) {
+		console.log('Posting Date of the Custom Script');
+
+		frm.set_value('reference_date', frm.doc.posting_date); // Equal Values
+	}
+
+});
