91d5da6fca0fe08618cd540d648c6db2c023c909
Jeans K. Real <jkrb7@hotmail.com>
2024-11-23 15:33:26 -0600

    Print Format Item Label With Price

--- gr_tools/grupo_real_tools/print_format/item_label_with_price/item_label_with_price.py
+++ gr_tools/grupo_real_tools/print_format/item_label_with_price/item_label_with_price.py
@@ -5,10 +5,14 @@ from erpnext.utilities.product import get_price
 @frappe.whitelist(allow_guest=False, methods='GET')
 def get_erpnext_price(item_code):
 	# This function is used to get the price of an item with the Pricing Rule Applied if available
-	selling_price_list = frappe.db.get_single_value('Selling Settings', 'selling_price_list')
+
 	# TODO: Webshop Price List
 	# TODO: Customer Price List?
-
+	selling_price_list = frappe.db.get_single_value('Selling Settings', 'selling_price_list')
 	default_company = frappe.defaults.get_user_default('Company')  # FIXME: This can be improved
 
-	return get_price(item_code, selling_price_list, customer_group='', company=default_company)
+	pricing = get_price(item_code, price_list=selling_price_list, customer_group='', company=default_company)
+
+	pricing.formatted_discount_percent = f"{pricing.discount_percent:.0f}%"
+
+	return pricing
