3b5e36bdc3fc85f6f8054a73e786df5d801d5993
Jeans K. Real <jkrb7@hotmail.com>
2024-11-25 08:22:54 -0600

    API for Product listing

---
+++ gr_tools/www/products.py
@@ -0,0 +1,28 @@
+import frappe
+from webshop.webshop.shopping_cart.product_info import set_product_info_for_website
+
+
+@frappe.whitelist(allow_guest=True)
+def get_products(start=0, limit=12):
+	query = """
+		SELECT
+			item.item_name,
+			bin.item_code,
+			bin.actual_qty
+		FROM
+			`tabBin` AS bin
+		JOIN `tabItem` AS item ON item.item_code = bin.item_code
+		WHERE
+			bin.actual_qty > 0
+			AND bin.warehouse = 'Tienda - CL'
+		ORDER BY
+			bin.item_code ASC;
+	"""
+
+	items = frappe.db.sql(query, as_dict=True)
+
+	for item in items:
+		set_product_info_for_website(item)
+
+	return items
+
