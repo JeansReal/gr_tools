424ff4f757b12fb36a852e813eab077e5467c519
Jeans K. Real <jkrb7@hotmail.com>
2024-11-25 11:57:47 -0600

    Headless Ecommerce Test

--- gr_tools/www/products.py
+++ gr_tools/www/products.py
@@ -3,7 +3,7 @@ from webshop.webshop.shopping_cart.product_info import set_product_info_for_webs
 
 
 @frappe.whitelist(allow_guest=True)
-def get_products(start=0, limit=12):
+def get_products(start=0, limit=15):
 	query = """
 		SELECT
 			item.item_name,
@@ -17,7 +17,7 @@ def get_products(start=0, limit=12):
 			bin.actual_qty > 0
 			AND bin.warehouse = 'Tienda - CL'
 		ORDER BY
-			item.creation ASC
+			item.creation DESC
 		LIMIT {start}, {limit};
 	""".format(start=start, limit=limit)
 
@@ -28,3 +28,36 @@ def get_products(start=0, limit=12):
 
 	return items
 
+
+@frappe.whitelist(allow_guest=True)
+def get_categories():
+	data = frappe.db.get_all(
+		'Item Group',
+		fields=['name', 'is_group as isLeaf', 'parent_item_group', 'idx as counter'],
+		filters={'show_in_website': 1}, order_by='is_group DESC'
+	)
+
+	frappe.log_error(title="Debug: Data without Filters", message=data)
+
+	# Diccionario para almacenar nodos y sus hijos
+	nodes = {}
+	tree = []
+
+	# Crear nodos y asociarlos a sus padres
+	for item in data:
+		nodes[item['name']] = {"key": item['name'].upper().replace(" ", "_"),
+			"value": {"label": item['name'], "counter": item['counter']}, "isLeaf": not item['isLeaf'],
+			# `is_group` indica si no es hoja
+			"children": []}
+
+	# Asignar relaciones jerárquicas
+	for item in data:
+		node = nodes[item['name']]
+		parent_name = item['parent_item_group']
+		if parent_name and parent_name in nodes:
+			nodes[parent_name]['children'].append(node)
+		else:
+			# Nodo raíz
+			tree.append(node)
+
+	return tree
