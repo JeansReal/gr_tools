ea971e53381e82336e5c0b622aa0d5ddda3d54a8
Jeans K. Real <jkrb7@hotmail.com>
2024-11-25 22:10:59 -0600

    Product List is more optimzied

--- gr_tools/www/products.py
+++ gr_tools/www/products.py
@@ -1,30 +1,49 @@
 import frappe
-from webshop.webshop.shopping_cart.product_info import set_product_info_for_website
+from erpnext.utilities.product import get_price
 
 
 @frappe.whitelist(allow_guest=True)
-def get_products(start=0, limit=15):
+def get_products(item_code: str = None, start: int = 0, limit: int = 15):
+	"""
+	Get a list of available products or a specific product by item_code.
+	Copy from: get_product_info_for_website
+
+	Parameters:
+		item_code (str): If provided, fetches only the specific product.
+		start (int): Pagination start index.
+		limit (int): Number of products to fetch.
+
+	Returns:
+		List of products or a single product.
+	"""
+
+	# TODO: Set a Default Warehouse, Maybe for each item?
+	# FIXME: Dont Show Reserved Stock!
+	# FIXME: Show BackOrder Products(For future sales or pre-orders)
+
 	query = """
 		SELECT
 			item.item_name,
 			item.image,
+			item.item_group,
 			bin.item_code,
 			bin.actual_qty
-		FROM
-			`tabBin` AS bin
+		FROM `tabBin` AS bin
 		JOIN `tabItem` AS item ON item.item_code = bin.item_code
-		WHERE
-			bin.actual_qty > 0
-			AND bin.warehouse = 'Tienda - CL'
-		ORDER BY
-			item.creation DESC
-		LIMIT {start}, {limit};
-	""".format(start=start, limit=limit)
+		WHERE bin.actual_qty > 0 AND bin.warehouse = 'Tienda - CL'
+	"""
 
-	items = frappe.db.sql(query, as_dict=True)
+	if item_code:
+		query += " AND item.item_code = %(item_code)s LIMIT 1"
+		items = frappe.db.sql(query, {'item_code': item_code}, as_dict=True)
+	else:
+		query += " ORDER BY item.creation DESC LIMIT %(start)s, %(limit)s;"
+		items = frappe.db.sql(query, {"start": start, "limit": limit}, as_dict=True)
+
+	[[price_list, company]] = frappe.db.get_values_from_single(['price_list', 'company'], None, 'WebShop Settings')
 
 	for item in items:
-		set_product_info_for_website(item)  # TODO: Optimize this. calling directly to get_price!
+		item.price = get_price(item.item_code, price_list=price_list, customer_group='', company=company)
 
 	return items
 
